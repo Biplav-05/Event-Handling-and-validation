9b4093286af161e3394ad805d91d9da80707f6a9
Biplav-05 <biplav413@gmail.com>
2022-09-14 14:03:22 +0545

    traditional mapper is replaced by model mapper

--- src/main/java/com/example/validation/ValidationAndExceptionHandlingApplication.java
+++ src/main/java/com/example/validation/ValidationAndExceptionHandlingApplication.java
@@ -1,13 +1,19 @@
 package com.example.validation;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ValidationAndExceptionHandlingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ValidationAndExceptionHandlingApplication.class, args);
+    }
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 
 }
--- src/main/java/com/example/validation/converter/StudentConverter.java
+++ src/main/java/com/example/validation/converter/StudentConverter.java
@@ -3,6 +3,8 @@ package com.example.validation.converter;
 import com.example.validation.controller.StudentController;
 import com.example.validation.model.Student;
 import com.example.validation.pojo.StudentPojo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,30 +13,35 @@ import java.util.stream.Collectors;
 @Component
 public class StudentConverter {
 
+    @Autowired
+    private ModelMapper modelMapper;
     public StudentPojo entityToDto(Student student)
     {
-        StudentPojo studentPojo = new StudentPojo();
-        studentPojo.setStudent_id(student.getStudent_id());
-        studentPojo.setFirst_name(student.getFirst_name());
-        studentPojo.setLast_name(student.getLast_name());
-        studentPojo.setAddress(student.getAddress());
+        StudentPojo studentPojo = this.modelMapper.map(student,StudentPojo.class);
+        //StudentPojo studentPojo = new StudentPojo();
+//        studentPojo.setStudent_id(student.getStudent_id());
+//        studentPojo.setFirst_name(student.getFirst_name());
+//        studentPojo.setLast_name(student.getLast_name());
+//        studentPojo.setAddress(student.getAddress());
         return studentPojo;
     }
     public List<StudentPojo> entityToDto(List<Student> student) {
-        return student.stream().map(e->entityToDto(e)).collect(Collectors.toList());
+       // return student.stream().map(e->entityToDto(e)).collect(Collectors.toList());
+      return   student.stream().map(a->modelMapper.map(a,StudentPojo.class)).collect(Collectors.toList());
     }
 
     public Student daoToEntity(StudentPojo studentPojo)
     {
-        Student student = new Student();
-        student.setStudent_id(studentPojo.getStudent_id());
-        student.setFirst_name(studentPojo.getFirst_name());
-        student.setLast_name(studentPojo.getLast_name());
-        student.setAddress(studentPojo.getAddress());
+        Student student = this.modelMapper.map(studentPojo,Student.class);
+//        student.setStudent_id(studentPojo.getStudent_id());
+//        student.setFirst_name(studentPojo.getFirst_name());
+//        student.setLast_name(studentPojo.getLast_name());
+//        student.setAddress(studentPojo.getAddress());
         return student;
     }
     public List<Student> daoToEntity(List<StudentPojo> studentPojo)
     {
-        return studentPojo.stream().map(a->daoToEntity(a)).collect(Collectors.toList());
+        //return studentPojo.stream().map(a->daoToEntity(a)).collect(Collectors.toList());
+        return studentPojo.stream().map(pojo->modelMapper.map(pojo,Student.class)).collect(Collectors.toList());
     }
 }
